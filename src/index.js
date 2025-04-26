65e8466c60a3e40a1f6483519412dfdcbbaf574a
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2025-04-26 16:12:26 -0600

    :construction_worker: Tipado y sus usos

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let numeros = [1, 2, 3]
+numeros.push('cuatro')
+
+// [1, 2, 3, 'cuatro']
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-let nombre: string = 'Carlos'
-let edad: number = 22
-let activo: boolean = true
+// Arreglos tipados
+let numeros: number[] = [1, 2, 3]
+numeros.push(4)
 
-console.log(`Nombre: ${nombre}, Edad: ${edad}, Activo: ${activo}`)
+// [1, 2, 3, 4]
+
+let nombres: Array<string> = ['Juan', 'Luis', 'Pedro']
+
+// Tuplas
+let persona: [string, number, boolean] = ['Juan', 25, true]
+
+persona[2] = false
+
+//Enums
+enum ColorSemaforo {
+    Rojo,
+    Verde,
+    Amarillo,
+}
+
+let estadoSemaforo: ColorSemaforo = ColorSemaforo.Amarillo
+
+console.log(estadoSemaforo)
+
+enum EstadoPedido {
+    Nuevo = 'NewOrder',
+    Procesando = 'InProgress',
+    Completado = 'Completed',
+}
+
+let estado: EstadoPedido = EstadoPedido.Completado
+console.log(estado)
+
+enum HttpStatus {
+    BadRequest = 400,
+    Forbiden = 503,
+    Success = 200,
+    Created = 201,
+}
+
+let responseStatus: HttpStatus = HttpStatus.Created
+console.log(responseStatus)
+// Union Types
+
+function imprimir(valor: string | number) {
+    console.log(valor)
+}
+
+function calcularLongitud(valor: string | any[]): number {
+    return valor.length
+}
